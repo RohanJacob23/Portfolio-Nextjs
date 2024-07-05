1b106fe2862d75bf83014d66bd072ce48496ad2e
RohanJacob23 <rohankoshyjacob@gmail.com>
2024-07-05 08:47:14 +0530

    sm: Made exclamation mark primary color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,9 @@ export default function Home() {
   return (
     <div className="flex-1 content-center space-y-4 p-4 md:p-8">
       <h1 className="flex flex-col gap-4">
-        <span>Hello!</span>
+        <span>
+          Hello<span className="text-primary">!</span>
+        </span>
         <span>
           I&apos;m{" "}
           <span className="underline decoration-primary">Rohan K. Jacob</span>
